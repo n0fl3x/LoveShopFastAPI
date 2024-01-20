4c8f8ca44d1d28c907d4a9d7c826f12fdd56e5a8
Roman Nevzorov <pomkanevzorov@gmail.com>
2024-01-21 00:12:37 +0600

    Endpoints and Enum classes updates.

--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,11 @@
 from random import choice
 
 from application import app
-from models import Categories, Items
+from models import (
+    RandomSomething,
+    Categories,
+    Items,
+)
 
 
 @app.get("/main")
@@ -12,18 +16,18 @@ async def get_main():
 
 @app.get("/categories")
 async def get_all_categories():
-    return {"all categories": [cat_name.value for cat_name in Categories]}
+    return {"all categories": [cat.name for cat in Categories]}
 
 
 @app.get("/categories/{category_id}")
-async def get_category(category_id: str = "random"):
-    if category_id == "random":
+async def get_category(category_id: Categories | RandomSomething):
+    if category_id is RandomSomething.random:
         random_category = choice(tuple(Categories))
-        return {"your random category": random_category}
+        return {"your random category": random_category.name}
 
-    categories_id = [cat.name for cat in Categories]
-    if category_id in categories_id:
-        output = {"category": Categories[category_id].value}
+    categories_vals = [cat.value for cat in Categories]
+    if Categories(category_id).value in categories_vals:
+        output = {"category": Categories(category_id).name}
         return output
     
     return {"error_message": "no such category"}
@@ -31,18 +35,18 @@ async def get_category(category_id: str = "random"):
 
 @app.get("/items")
 async def get_all_items():
-    return {"all items": [item_name.value for item_name in Items]}
+    return {"all items": [item.name for item in Items]}
 
 
 @app.get("/items/{item_id}")
-async def get_item(item_id: str = "random"):
-    if item_id == "random":
+async def get_item(item_id: Items | RandomSomething):
+    if item_id is RandomSomething.random:
         random_item = choice(tuple(Items))
-        return {"your random item": random_item}
+        return {"your random item": random_item.name}
     
-    items_id = [item.name for item in Items]
-    if item_id in items_id:
-        output = {"item": Items[item_id].value}
+    items_vals = [item.value for item in Items]
+    if Items(item_id).value in items_vals:
+        output = {"item": Items(item_id).name}
         return output
     
     return {"error_message": "no such item"}
--- models.py
+++ models.py
@@ -1,13 +1,21 @@
-from enum import Enum
+from enum import Enum, unique
 
 
-class Categories(str, Enum):
-    cat_1 = "lubes"
-    cat_2 = "dildos"
-    cat_3 = "vibrators"
+class RandomSomething(str, Enum):
+    random = "random"
 
 
-class Items(str, Enum):
-    item_1 = "water based lube"
-    item_2 = "bodysuit"
-    item_3 = "butt plug"
+@unique
+class Categories(int, Enum):
+    lubes = 1
+    dildos = 2
+    vibrators = 3
+    giants = 4
+    
+
+@unique
+class Items(int, Enum):
+    water_based_lube = 1
+    bodysuit = 2
+    butt_plug = 3
+    horse_cock = 4
